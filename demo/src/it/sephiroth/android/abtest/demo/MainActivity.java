0efd3cb0abdfba61504ec37efbbdc86a3e724b8c
Alessandro Crugnola <alessandro@aviary.com>
2013-11-19 11:41:21 -0500

    gradle

--- demo/src/it/sephiroth/android/abtest/demo/MainActivity.java
+++ demo/src/it/sephiroth/android/abtest/demo/MainActivity.java
@@ -3,9 +3,7 @@ package it.sephiroth.android.abtest.demo;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import it.sephiroth.android.library.ab.AB;
 import it.sephiroth.android.library.ab.ABTest;
 
